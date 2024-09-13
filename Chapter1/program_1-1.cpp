9108a46a38a3c9b77f6ca3bb4d51b3d5f87591ab
resego-morei <resego.morei@gmail.com>
2024-09-13 12:48:00 +0200

    chapter 1 program

---
+++ Chapter1/program_1-1.cpp
@@ -0,0 +1,14 @@
+// It's a mad, mad program
+#include <iostream>
+
+using namespace std;
+
+int main(){
+    unsign short int numbers;
+    
+    cout << "Success\n";
+    cout << "Success";
+    cout << " Success\n\n";
+    cout << "Success\n";
+    return 0;
+}
